30343f70566a7bba36f85b69f29ceea14e5e9a25
black-jamaesbond <kshedu197@gmail.com>
2024-06-05 02:39:29 +0100

    second update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,5 +13,4 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
-
     return response.json().get('data').get('subscribers')
